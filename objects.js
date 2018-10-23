59109955037c3b97ec6d4f0abfc8fa89b0a15497
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-23 21:03:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,3 @@
 var playlist = {
   'The Who': 'Teenage Wasteland'
 }
-
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist['Phil Ochs'] = 'Slowdive'
-  return playlist
-}
-
-function removeFromPlaylist(playlist, artistName){
-  delete playlist['Slowdive']
-  return playlist
-}
